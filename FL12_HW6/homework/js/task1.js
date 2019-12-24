99a197e5a21fbd7c03f9e7e5ad00436f0349f2b7
ziovio <anderkoval3@gmail.com>
2019-12-24 16:10:17 +0200

    hw6 done

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -4,11 +4,11 @@ const b = parseInt(prompt('Enter b'));
 const c = parseInt(prompt('Enter c'));
 
 if ([a, b, c].some(isNaN)) {
-    console.log('invalid input data');
+    console.log('Invalid input data');
 } else {
     const d = b*b - 4*a*c;
     if (d < 0) {
-        console.log(`No solution: Discriminant - ${d} is less then 0`);
+        console.log(`No solution: Discriminant (${d}) is less then 0`);
     } else {
         if (d === 0) {
             const x = -b / 2*a; 
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,22 @@
-// Your code goes here
+const a = parseInt(prompt('Enter a'));
+const b = parseInt(prompt('Enter b'));
+const c = parseInt(prompt('Enter c'));
+
+const sides = [a, b, c];
+
+if (sides.some(isNaN)) {
+    alert('input values should be ONLY numbers');
+} else if (sides.some((s) => s <= 0 )) {
+    alert('A triangle must have 3 sides with a positive definite length ');
+} else if (a + b < c || a + c < b || b + c < a) {
+    alert('Triangle doesn’t exist');
+} else {
+
+    if (a === b && b === c) {
+        console.log('Equilateral triangle');
+    } else if (sides.some((s, index) => s === sides[(index + 1) % sides.length])) {
+        console.log('Isosceles triangle');
+    } else if (a !== b && b !== c && a !== c) {
+        console.log('Scalene triangle');
+    }
+}
