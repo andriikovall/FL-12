f1bebc0b21fc9313b07bb36eab829552da26e39a
ziovio <anderkoval3@gmail.com>
2020-01-19 12:21:46 +0200

    a bit refactored to make it a more clear and in right way

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,4 +1,4 @@
-const Fighter = function({ name, damage, strength, agility, hp }) {
+const Fighter = function ({ name, damage, strength, agility, hp }) {
     // probably should check input values but it wasnt mentioned in task 
     // and we can assume that all inputs are valid
     const maxHP = hp;
@@ -6,47 +6,45 @@ const Fighter = function({ name, damage, strength, agility, hp }) {
         wins: 0,
         losses: 0
     };
-    return new function() {
-        this.getName = () => name;
-        this.getDamage = () => damage;
-        this.getStrength = () => strength;
-        this.getAgility = () => agility;
-        this.getHealth = () => hp;
-        this.heal = (hpToHeal) => {
-            hp += hpToHeal;
-            if (hp > maxHP) {
-                hp = maxHP;
-            }
-        };
-        this.dealDamage = (dmg) => {
-            hp -= dmg;
-            if (hp < 0) {
-                hp = 0;
-            }
-        };
-        this.addWin = () => {
-            combatHistory.wins += 1
-        };
-        this.addLoss = () => {
-            combatHistory.losses += 1
-        };
-        this.logCombatHistory = () => {
-            const message = `Name: ${name}, Wins: ${combatHistory.wins}, Losses: ${combatHistory.losses}`;
-            console.log(message);
-        }, 
-        this.attack = (enemy) => {
-            const oneHundred = 100;
-            const successProbability = 1 - (enemy.getStrength() + enemy.getAgility()) / oneHundred;
-            const isAttackSuccess = Math.random() <= successProbability;
-            if (isAttackSuccess) {
-                const attackerDmg = this.getDamage();
-                enemy.dealDamage(attackerDmg);
-                console.log(`${this.getName()} makes ${attackerDmg} damage to ${enemy.getName()}`);
-            } else {
-                console.log(`${this.getName()} attack missed`);
-            }
+    this.getName = () => name;
+    this.getDamage = () => damage;
+    this.getStrength = () => strength;
+    this.getAgility = () => agility;
+    this.getHealth = () => hp;
+    this.heal = (hpToHeal) => {
+        hp += hpToHeal;
+        if (hp > maxHP) {
+            hp = maxHP;
         }
-    }()
+    };
+    this.dealDamage = (dmg) => {
+        hp -= dmg;
+        if (hp < 0) {
+            hp = 0;
+        }
+    };
+    this.addWin = () => {
+        combatHistory.wins += 1
+    };
+    this.addLoss = () => {
+        combatHistory.losses += 1
+    };
+    this.logCombatHistory = () => {
+        const message = `Name: ${name}, Wins: ${combatHistory.wins}, Losses: ${combatHistory.losses}`;
+        console.log(message);
+    };
+    this.attack = (enemy) => {
+        const oneHundred = 100;
+        const successProbability = 1 - (enemy.getStrength() + enemy.getAgility()) / oneHundred;
+        const isAttackSuccess = Math.random() <= successProbability;
+        if (isAttackSuccess) {
+            const attackerDmg = this.getDamage();
+            enemy.dealDamage(attackerDmg);
+            console.log(`${this.getName()} makes ${attackerDmg} damage to ${enemy.getName()}`);
+        } else {
+            console.log(`${this.getName()} attack missed`);
+        }
+    };
 }
 
 const fighter1 = new Fighter({ name: 'Andrey', damage: 15, strength: 10, agility: 50, hp: 130 });
